a0beef8592bed6456c0d245891645e7d0631e015
Gabriel Marcial <gabrielmarcial@Gabriels-MacBook-Air.local>
2023-04-24 20:50:53 -0400

    Add a working version of GoogleSearchService

--- src/main/python/WordsToMaster.py
+++ src/main/python/WordsToMaster.py
@@ -4,6 +4,12 @@ class WordsToMaster:
 
     def startApplication(self): 
         print("Welcome to WordsToMaster, your very own english literacy tool! Brought to you by big daddy Gabe.")
+
+        # New user interface
+
+        # New storage container tha will be empty or import 
+
+        # New google search service
         
             
 
--- src/main/python/service/GoogleSearchService.py
+++ src/main/python/service/GoogleSearchService.py
@@ -1,3 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
 class GoogleSearchService:
     def __init__(self) -> None:
         pass
+
+    def getDefinition(self, word):
+        url = f"https://www.google.com/search?q=define+{word}"
+        headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36"}
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, "html.parser")
+        #print(soup.prettify())
+        definition = None
+        for tag in soup.find_all("div"):
+            if "data-dobid" in tag.attrs and tag.attrs["data-dobid"] == "dfn":
+                definition = tag.get_text()
+                break
+        return definition
+
+
+
+m = GoogleSearchService()
+
+word = "ferocious"
+definition = m.getDefinition(word)
+if definition:
+    print(f"The definition of '{word}' is '{definition}'")
+else:
+    print(f"No definition found for '{word}'")
+
